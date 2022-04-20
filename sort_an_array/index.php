3469371eca7f82c5e47347dc5a2bde03590d2f06
Adeola Ephraim <dunsin.bada@gmail.com>
2022-04-20 09:13:02 +0100

    added sort an array

--- sort_an_array/index.php
+++ sort_an_array/index.php
@@ -1,8 +1,8 @@
 <?php
 
 $my_array = [12, 14, 1, 9, 8, 3, 15, 7, 8, 45, 20];
-$sorted_array = $my_array;
-sort($sorted_array, SORT_NUMERIC);
+$sorted_array = $my_array; // Copy array
+sort($sorted_array, SORT_NUMERIC); // sort copied array
 echo "unsorted array - " .json_encode($my_array);
 echo "<br>";
 echo "sorted array - " .json_encode($sorted_array);
